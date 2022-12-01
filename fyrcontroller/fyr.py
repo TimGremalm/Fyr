55e730c1420f6c8906dbbd6f7f45912e439b0360
Tim Gremalm <tim@gremalm.se>
2022-12-02 00:57:01 +0100

    Coordinate fetcher

--- fyrcontroller/fyr.py
+++ fyrcontroller/fyr.py
@@ -3,6 +3,74 @@ from time import sleep, time
 from enum import Enum
 from pythonosc.udp_client import SimpleUDPClient
 import colorsys
+import numpy as np
+
+def vector_calc(lat, long, ht):
+    '''
+    Calculates the vector from a specified point on the Earth's surface to the North Pole.
+    '''
+    a = 6378137.0  # Equatorial radius of the Earth
+    b = 6356752.314245  # Polar radius of the Earth
+
+    e_squared = 1 - ((b ** 2) / (a ** 2))  # e is the eccentricity of the Earth
+    n_phi = a / (np.sqrt(1 - (e_squared * (np.sin(lat) ** 2))))
+
+    x = (n_phi + ht) * np.cos(lat) * np.cos(long)
+    y = (n_phi + ht) * np.cos(lat) * np.sin(long)
+    z = ((((b ** 2) / (a ** 2)) * n_phi) + ht) * np.sin(lat)
+
+    x_npole = 0.0
+    y_npole = 6378137.0
+    z_npole = 0.0
+
+    v = ((x_npole - x), (y_npole - y), (z_npole - z))
+
+    return v
+
+def angle_calc(lat1, long1, lat2, long2, ht1=0, ht2=0):
+    '''
+    Calculates the angle between the vectors from 2 points to the North Pole.
+    '''
+    # Convert from degrees to radians
+    lat1_rad = (lat1 / 180) * np.pi
+    long1_rad = (long1 / 180) * np.pi
+    lat2_rad = (lat2 / 180) * np.pi
+    long2_rad = (long2 / 180) * np.pi
+
+    v1 = vector_calc(lat1_rad, long1_rad, ht1)
+    v2 = vector_calc(lat2_rad, long2_rad, ht2)
+
+    # The angle between two vectors, vect1 and vect2 is given by:
+    # arccos[vect1.vect2 / |vect1||vect2|]
+    dot = np.dot(v1, v2)  # The dot product of the two vectors
+    v1_mag = np.linalg.norm(v1)  # The magnitude of the vector v1
+    v2_mag = np.linalg.norm(v2)  # The magnitude of the vector v2
+
+    theta_rad = np.arccos(dot / (v1_mag * v2_mag))
+    # Convert radians back to degrees
+    theta = (theta_rad / np.pi) * 180
+
+    return theta
+
+class CoordinateFetcher(threading.Thread):
+    def __init__(self, fyr, sector):
+        self.fyr = fyr
+        self.sector = sector
+        self.fyr.fetcher = self
+
+        threading.Thread.__init__(self)
+        self.setDaemon(True)
+        self.quit = False
+
+    def fetch(self):
+        pass
+
+    def run(self):
+        while True:
+            if self.quit:
+                return
+            self.fetch()
+            sleep(2)
 
 class Sector(threading.Thread):
     def __init__(self, fyr):
@@ -137,6 +205,9 @@ if __name__ == '__main__':
     s = Sector(f)
     s.start()
 
+    c = CoordinateFetcher(f, s)
+    c.start()
+
     banner = [f"{title_long} Shell",
               'You may leave this shell by typing `exit`, `q` or pressing Ctrl+D']
     Pysh(dict_to_include={'fyr': f},
@@ -144,4 +215,5 @@ if __name__ == '__main__':
          banner=banner)
     f.quit = True
     s.quit = True
+    c.quit = True
 
